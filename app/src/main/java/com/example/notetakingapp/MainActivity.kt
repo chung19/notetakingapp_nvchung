1838fe7b766956d6fa857bb3bba7ae0170396b3a
chung19 <nvchung141219@gmail.com>
2024-06-25 14:37:20 +0700

    update home and  new note fragments

--- app/src/main/java/com/example/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapp/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-  binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.fragmentContainerView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
